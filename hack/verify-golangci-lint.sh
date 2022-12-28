a575a1a7f889d03c902513ea9d68bac372bcfafe
jiangjie <670884108@qq.com>
2022-12-28 15:40:54 +0800

    fix golangci-lint on ubuntu problem

--- hack/verify-golangci-lint.sh
+++ hack/verify-golangci-lint.sh
@@ -16,7 +16,7 @@ fi
 # if ! command -v gofumpt; then
 #     go install mvdan.cc/gofumpt@v0.3.1
 # fi
-
+export CGO_ENABLED=0
 go get github.com/golangci/golangci-lint/cmd/golangci-lint@v1.42.0
 if ! command -v golangci-lint; then
     curl -sfL $URL | sh -s $VERSION
