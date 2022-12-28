001fad4bf4e6277998c921cb4e9f2a1a0c18f8ea
jiangjie <670884108@qq.com>
2022-12-28 16:34:51 +0800

    test golangci-lint in jenkins that runned on docker

--- hack/verify-golangci-lint.sh
+++ hack/verify-golangci-lint.sh
@@ -16,12 +16,14 @@ fi
 if ! command -v gofumpt; then
     go install mvdan.cc/gofumpt@v0.3.1
 fi
+
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s v1.46.2
 # export CGO_ENABLED=0
 # go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.42.0
-if ! command -v golangci-lint; then
-    curl -sfL $URL | sh -s $VERSION
-    PATH=$PATH:bin
-fi
+# if ! command -v golangci-lint; then
+#     curl -sfL $URL | sh -s $VERSION
+#     PATH=$PATH:bin
+# fi
 
 golangci-lint version
 golangci-lint linters
