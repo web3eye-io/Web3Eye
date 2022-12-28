77cc836e81e69b95a20bb18ab7ff2c3373c64da5
jiangjie <670884108@qq.com>
2022-12-28 15:26:01 +0800

    fix golangci-lint on ubuntu problem

--- hack/verify-golangci-lint.sh
+++ hack/verify-golangci-lint.sh
@@ -4,9 +4,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-VERSION=v1.46.0
-
-URL=github.com/golangci/golangci-lint/cmd/golangci-lint@$VERSION
+VERSION=v1.46.2
+URL_BASE=https://raw.githubusercontent.com/golangci/golangci-lint
+URL=$URL_BASE/$VERSION/install.sh
 
 if [[ ! -f .golangci.yml ]]; then
     echo 'ERROR: missing .golangci.yml in repo root' >&2
@@ -17,11 +17,10 @@ if ! command -v gofumpt; then
     go install mvdan.cc/gofumpt@v0.3.1
 fi
 
-# if ! command -v golangci-lint; then
-export CGO_ENABLED=0
-go install $URL
-PATH=$PATH:bin
-# fi
+if ! command -v golangci-lint; then
+    curl -sfL $URL | sh -s $VERSION
+    PATH=$PATH:bin
+fi
 
 golangci-lint version
 golangci-lint linters
