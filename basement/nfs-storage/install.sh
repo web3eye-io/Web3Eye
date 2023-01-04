3cf62761597bbda17ee1663ec6d795006bfa1b1b
jiangjie <670884108@qq.com>
2023-01-04 20:12:48 +0800

    add nfs-storage-privisioner

--- basement/nfs-storage/install.sh
+++ basement/nfs-storage/install.sh
@@ -15,6 +15,6 @@ set -o pipefail
 helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner
 helm repo update
 
-helm install default_nfs_provisioner \
+helm install default-nfs-provisioner \
     nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
     -f $SHELL_FOLDER/value.yaml
