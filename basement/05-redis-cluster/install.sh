54d795321e7d01b45507c483ddc6feb14f42cecc
jiangjie <670884108@qq.com>
2023-01-06 18:35:37 +0800

    add secret for redis-cluster

--- basement/05-redis-cluster/install.sh
+++ basement/05-redis-cluster/install.sh
@@ -13,8 +13,5 @@ set -o nounset
 set -o pipefail
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install redis-cluster --set redis.password="default" bitnami/redis-cluster
+helm install redis-cluster bitnami/redis-cluster
 kubectl get pods | grep redis
-
-# TODO: bitnami安装redis默认没生成secret，需要手动生成一个，提供密码
-# helm install redis --set auth.password="q123" bitnami/redis
