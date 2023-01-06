c6aa118f69988e0b7a45be3547ce59d8a3b0456c
jiangjie <670884108@qq.com>
2023-01-06 13:23:09 +0800

    fix bug of deploy-to-k8s for image-converter

--- image-converter/init.py
+++ image-converter/init.py
@@ -1,4 +1,4 @@
 from pkg.model.resnet50 import Resnet50
 
 # Load the operator ahead of time
-Resnet50().resnet50_extract_feat(img_path="./tmp/milvus.jpg")
+Resnet50().resnet50_extract_feat(img_path="./img/milvus.jpg")
