bb41f9af7b919b689b66dfd5411dededcbe81a7d
jiangjie <670884108@qq.com>
2023-01-09 10:57:59 +0800

    change 'kubectl apply' to 'kubectl replace'

--- block-etl/script/deploy-to-k8s-cluster.sh
+++ block-etl/script/deploy-to-k8s-cluster.sh
@@ -50,4 +50,9 @@ echo "Deploy docker image for $PLATFORM -- $version"
 sed -i "s/$service_name:latest/$service_name:$version/g" $ROOT_FOLDER/cmd/$service_name/k8s/02-$service_name.yaml
 # sed -i "s/uhub.service.ucloud.cn/$DOCKER_REGISTRY/g" cmd/$service_name/k8s/02-$service_name.yaml
 
-kubectl replace -k $ROOT_FOLDER/cmd/$service_name/k8s
+kubectl get deployment | grep $service_name
+if [ ! $? -eq 0 ]; then
+  kubectl apply -k $ROOT_FOLDER/cmd/$service_name/k8s
+else
+  kubectl replace -k $ROOT_FOLDER/cmd/$service_name/k8s
+fi
