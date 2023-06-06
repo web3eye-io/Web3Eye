4e67ffccd773825bf1369615f3ecef57d409adee
Geapefurit <670884108@qq.com>
2023-06-06 19:29:37 +0800

    support parse config.toml

--- image-converter/script/build-docker-image.sh
+++ image-converter/script/build-docker-image.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-SHELL_FOLDER=$(
-  cd "$(dirname "$0")"
-  pwd
-)
-ROOT_FOLDER=$(
-  cd $SHELL_FOLDER/../
-  pwd
-)
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+ROOT_FOLDER=$(cd $SHELL_FOLDER/../;pwd)
+PROJECT_FOLDER=$(cd $ROOT_FOLDER/../;pwd)
+
 set -o errexit
 set -o nounset
 set -o pipefail
+
+configFile=$PROJECT_FOLDER/config/config.toml
+myConfigPath=$ROOT_FOLDER/pkg/utils/
+echo "sync config.toml from $configFile to $myConfigPath"
+cp $configFile $myConfigPath
 
 if git_status=$(git status --porcelain --untracked=no 2>/dev/null) && [[ -z "${git_status}" ]]; then
   git_tree_state=clean
--- image-converter/script/build.sh
+++ image-converter/script/build.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 ROOT_FOLDER=$(cd $SHELL_FOLDER/../;pwd)
+PROJECT_FOLDER=$(cd $ROOT_FOLDER/../;pwd)
 
 set -o errexit
 set -o nounset
 set -o pipefail
 
-echo "Nothing to do"
+configFile=$PROJECT_FOLDER/config/config.toml
+myConfigPath=$ROOT_FOLDER/pkg/utils/
+echo "sync config.toml from $configFile to $myConfigPath"
+cp $configFile $myConfigPath
+
 cd $ROOT_FOLDER
