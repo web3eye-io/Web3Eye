cc5a4d136c731c40101cacca3eca48e168da587f
root <root@development-box-1.development-box.kube-system.svc.cluster.local>
2023-06-07 07:46:27 +0000

    Add run to dealer

--- dealer/cmd/dealer/run.go
+++ dealer/cmd/dealer/run.go
@@ -1,20 +1,36 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	cli "github.com/urfave/cli/v2"
+	"github.com/web3eye-io/Web3Eye/common/servermux"
 	"github.com/web3eye-io/Web3Eye/config"
 
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	api "github.com/web3eye-io/Web3Eye/cloud-proxy/api/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/reflection"
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 var runCmd = &cli.Command{
 	Name:    "run",
 	Aliases: []string{"r"},
-	Usage:   "Run dealer daemon",
+	Usage:   "Run Dealer daemon",
 	After: func(c *cli.Context) error {
 		return logger.Sync()
 	},
@@ -22,12 +38,46 @@ var runCmd = &cli.Command{
 		return logger.Init(logger.DebugLevel, config.GetConfig().Dealer.LogFile)
 	},
 	Action: func(c *cli.Context) error {
+		go runHTTPServer(config.GetConfig().Dealer.HTTPPort, config.GetConfig().Dealer.GrpcPort)
+		go runGRPCServer(config.GetConfig().Dealer.GrpcPort)
 		sigchan := make(chan os.Signal, 1)
 		signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
 
 		<-sigchan
 		os.Exit(1)
-
 		return nil
 	},
+}
+
+func runGRPCServer(grpcPort int) {
+	endpoint := fmt.Sprintf(":%v", grpcPort)
+	lis, err := net.Listen("tcp", endpoint)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	server := grpc.NewServer()
+	api.Register(server)
+	reflection.Register(server)
+	if err := server.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+func runHTTPServer(httpPort, grpcPort int) {
+	httpEndpoint := fmt.Sprintf(":%v", httpPort)
+	grpcEndpoint := fmt.Sprintf(":%v", grpcPort)
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	err := api.RegisterGateway(mux, grpcEndpoint, opts)
+	if err != nil {
+		log.Fatalf("Fail to register gRPC gateway service endpoint: %v", err)
+	}
+
+	servermux.AppServerMux().Handle("/v1/", mux)
+	err = http.ListenAndServe(httpEndpoint, servermux.AppServerMux())
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
 }
