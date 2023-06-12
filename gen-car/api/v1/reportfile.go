347f15d9127b60a4f7573a644ec179d3702a6d43
Geapefurit <670884108@qq.com>
2023-06-12 11:57:30 +0800

    use milvus-minio

---
+++ gen-car/api/v1/reportfile.go
@@ -0,0 +1,141 @@
+//go:build !codeanalysis
+// +build !codeanalysis
+
+package v1
+
+import (
+	"context"
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"path"
+
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	"github.com/mr-tron/base58/base58"
+	"github.com/web3eye-io/Web3Eye/common/oss"
+	"github.com/web3eye-io/Web3Eye/nft-meta/pkg/client/v1/token"
+	v1 "github.com/web3eye-io/Web3Eye/proto/web3eye/gencar/v1"
+)
+
+const (
+	ImageResType = "image"
+	ImageTempDir = "./img"
+)
+
+type TokenBaseInfo struct {
+	ChainType string
+	ChainID   string
+	Contract  string
+	TokenID   string
+}
+
+type TokenResInfo struct {
+	BaseInfo TokenBaseInfo
+	FileName string
+	Size     int64
+	S3Key    string
+	ID       string
+	ResType  string
+}
+
+func (s *Server) ReportFile(ctx context.Context, in *v1.ReportFileRequest) (*v1.ReportFileResponse, error) {
+	logger.Sugar().Infof("start deal file, id %v, s3key %v", in.ID, in.S3Key)
+	info, err := token.GetToken(ctx, in.ID)
+	if err != nil {
+		return nil, err
+	}
+	objAttr, err := oss.GetObjectAttributes(ctx, in.S3Key)
+	if err != nil {
+		return nil, err
+	}
+	resInfo := &TokenResInfo{
+		BaseInfo: TokenBaseInfo{
+			ChainType: info.ChainType.String(),
+			ChainID:   info.ChainID,
+			Contract:  info.Contract,
+			TokenID:   info.TokenID,
+		},
+		S3Key:   in.S3Key,
+		ID:      in.ID,
+		ResType: ImageResType,
+		Size:    objAttr.ContentLength,
+	}
+	sha2sum, err := AnySHA256Sum(resInfo.BaseInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	resInfo.FileName = fmt.Sprintf("%v.%v", sha2sum, path.Ext(in.S3Key))
+
+	carManager := GetCarManager()
+	carManager.PutTokenResInfo(resInfo)
+
+	return &v1.ReportFileResponse{
+		ID:    in.ID,
+		S3Key: in.S3Key,
+	}, nil
+}
+
+func AnySHA256Sum(obj any) (string, error) {
+	rawObj, err := json.Marshal(obj)
+	if err != nil {
+		return "", err
+	}
+	_h := sha256.Sum256(rawObj)
+	h := base58.Encode(_h[:])
+	return h, nil
+}
+
+func CheckAnySHA256Sum(obj any, base58str string) (bool, error) {
+	h, err := AnySHA256Sum(obj)
+	if err != nil {
+		return false, err
+	}
+	if h == base58str {
+		return true, nil
+	}
+	return false, nil
+}
+
+type CarManager struct {
+	downloadChan  chan *TokenResInfo
+	downloadClose chan struct{}
+}
+
+const (
+	DefaultDownloadChanLen  = 100
+	DefaultDownloadParallel = 3
+)
+
+var carManager *CarManager
+
+func GetCarManager() *CarManager {
+	if carManager != nil {
+		return carManager
+	}
+	carManager = &CarManager{
+		downloadChan:  make(chan *TokenResInfo, DefaultDownloadChanLen),
+		downloadClose: make(chan struct{}),
+	}
+	carManager.runDownloadTask(DefaultDownloadParallel)
+	return carManager
+}
+
+func (cm *CarManager) PutTokenResInfo(tokenResInfo *TokenResInfo) {
+	cm.downloadChan <- tokenResInfo
+}
+
+func (cm *CarManager) runDownloadTask(parallel int) {
+	for i := 0; i < parallel; i++ {
+		go func() {
+			for {
+				info := <-cm.downloadChan
+				err := oss.DownloadFile(context.Background(), info.FileName, info.S3Key)
+				if err != nil {
+					logger.Sugar().Errorf("failed to download file from s3, err: %v", err)
+				}
+			}
+		}()
+	}
+	<-cm.downloadClose
+}
