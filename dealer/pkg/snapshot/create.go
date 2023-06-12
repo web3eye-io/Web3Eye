0b6f6d3b8adb83ab7f4a860beebb8aeb3f45cca4
Zhao KK <kikakkz@hotmail.com>
2023-06-12 14:49:59 +0800

    Support create snapshot

--- dealer/pkg/snapshot/create.go
+++ dealer/pkg/snapshot/create.go
@@ -1,5 +1,17 @@
 package snapshot
 
-func (h *Handler) CreateSnapshot(ctx context.Context) error {
+import (
+	"context"
 
+	orbit "github.com/web3eye-io/Web3Eye/dealer/pkg/orbit"
+)
+
+func (h *Handler) CreateSnapshot(ctx context.Context) error {
+	if err := orbit.Snapshot().SetWaitSnapshot(ctx, h.SnapshotURI, h.Items); err != nil {
+		return err
+	}
+	if err := orbit.Snapshot().NextWaitSnapshot(ctx); err != nil {
+		return err
+	}
+	return nil
 }
