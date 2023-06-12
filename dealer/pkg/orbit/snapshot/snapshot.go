11629478bece657424b7b489de8c83864f971472
Zhao KK <kikakkz@hotmail.com>
2023-06-12 14:41:08 +0800

    Support save meta of snapshot

--- dealer/pkg/orbit/snapshot/snapshot.go
+++ dealer/pkg/orbit/snapshot/snapshot.go
@@ -3,13 +3,17 @@ package snapshot
 import (
 	"context"
 	"encoding/binary"
+	"encoding/json"
 	"fmt"
+
+	dealerpb "github.com/web3eye-io/Web3Eye/proto/web3eye/dealer/v1"
 
 	orbitdb "berty.tech/go-orbit-db"
 	orbitiface "berty.tech/go-orbit-db/iface"
 )
 
 type SnapshotKV struct {
+	odb                 orbitiface.OrbitDB
 	kvSnapshotIndex     orbitdb.KeyValueStore
 	waitSnapshotIndex   uint64
 	backupSnapshotIndex uint64
@@ -18,11 +22,17 @@ type SnapshotKV struct {
 const (
 	CurrentWaitSnapshotIndex   = "current-wait-index"
 	CurrentBackupSnapshotIndex = "current-wait-index"
+	SnapshotURI                = "snapshot-url"
+	ContentItems               = "content-items"
 	KVStoreSnapshotIndex       = "snapshot-index"
+	KVStoreWaitSnapshot        = "wait-snapshot-"
+	KVStoreBackupSnapshot      = "backup-snapshot-"
 )
 
 func NewSnapshotKV(ctx context.Context, odb orbitiface.OrbitDB) (*SnapshotKV, error) {
-	kv := &SnapshotKV{}
+	kv := &SnapshotKV{
+		odb: odb,
+	}
 	var err error
 
 	replicate := true
@@ -67,6 +77,75 @@ func (kv *SnapshotKV) NextWaitSnapshot(ctx context.Context) error {
 	return nil
 }
 
+func (kv *SnapshotKV) setSnapshot(ctx context.Context, kvStoreName, snapshotURI string, items []*dealerpb.ContentItem) error {
+	replicate := true
+	_kv, err := kv.odb.KeyValue(ctx, kvStoreName, &orbitdb.CreateDBOptions{
+		Replicate: &replicate,
+	})
+	if err != nil {
+		return err
+	}
+	defer _kv.Close()
+
+	if err := _kv.Load(ctx, -1); err != nil {
+		return err
+	}
+
+	if _, err := _kv.Put(ctx, SnapshotURI, []byte(snapshotURI)); err != nil {
+		return err
+	}
+
+	b, err := json.Marshal(items)
+	if err != nil {
+		return err
+	}
+
+	if _, err := _kv.Put(ctx, ContentItems, b); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (kv *SnapshotKV) SetWaitSnapshot(ctx context.Context, snapshotURI string, items []*dealerpb.ContentItem) error {
+	return kv.setSnapshot(ctx, fmt.Sprintf("%v%v", KVStoreWaitSnapshot, kv.waitSnapshotIndex), snapshotURI, items)
+}
+
+func (kv *SnapshotKV) getSnapshot(ctx context.Context, kvStoreName string) (snapshotURI string, items []*dealerpb.ContentItem, err error) {
+	replicate := true
+	_kv, err := kv.odb.KeyValue(ctx, kvStoreName, &orbitdb.CreateDBOptions{
+		Replicate: &replicate,
+	})
+	if err != nil {
+		return "", nil, err
+	}
+	defer _kv.Close()
+
+	if err := _kv.Load(ctx, -1); err != nil {
+		return "", nil, err
+	}
+
+	_snapshotURI, err := _kv.Get(ctx, SnapshotURI)
+	if err != nil {
+		return "", nil, err
+	}
+
+	_items, err := _kv.Get(ctx, ContentItems)
+	if err != nil {
+		return "", nil, err
+	}
+
+	if err := json.Unmarshal(_items, &items); err != nil {
+		return "", nil, err
+	}
+
+	return string(_snapshotURI), items, nil
+}
+
+func (kv *SnapshotKV) GetWaitSnapshot(ctx context.Context) (snapshotURI string, items []*dealerpb.ContentItem, err error) {
+	return kv.getSnapshot(ctx, fmt.Sprintf("%v%v", KVStoreWaitSnapshot, kv.waitSnapshotIndex))
+}
+
 func (kv *SnapshotKV) NextBackupSnapshot(ctx context.Context) error {
 	if kv.kvSnapshotIndex == nil {
 		return fmt.Errorf("invalid kvstore")
@@ -80,6 +159,14 @@ func (kv *SnapshotKV) NextBackupSnapshot(ctx context.Context) error {
 	}
 
 	return nil
+}
+
+func (kv *SnapshotKV) SetBackupSnapshot(ctx context.Context, snapshotURI string, items []*dealerpb.ContentItem) error {
+	return kv.setSnapshot(ctx, fmt.Sprintf("%v%v", KVStoreBackupSnapshot, kv.backupSnapshotIndex), snapshotURI, items)
+}
+
+func (kv *SnapshotKV) GetBackupSnapshot(ctx context.Context) (snapshotURI string, items []*dealerpb.ContentItem, err error) {
+	return kv.getSnapshot(ctx, fmt.Sprintf("%v%v", KVStoreBackupSnapshot, kv.backupSnapshotIndex))
 }
 
 func (kv *SnapshotKV) Close() {
