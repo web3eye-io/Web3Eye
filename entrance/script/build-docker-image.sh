0ffdcc97ae8e16f8a8fff3ada49dd8b3a0805c05
Geapefurit <670884108@qq.com>
2023-06-12 12:05:12 +0800

    change wrong script

--- entrance/script/build-docker-image.sh
+++ entrance/script/build-docker-image.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 PROJECT_FOLDER=$(cd $SHELL_FOLDER/../;pwd)
-PROJECT_FOLDER=$(cd $PROJECT_FOLDER/../;pwd)
+ROOT_FOLDER=$(cd $PROJECT_FOLDER/../;pwd)
 
 set -o errexit
 set -o nounset
