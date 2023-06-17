a6a6788c6b321fd498932073315587a393bb3950
crazyzpl <crazyzplzpl@163.com>
2023-06-17 20:37:09 +0800

    Support retrieve

--- dealer/pkg/retrieve/stat.go
+++ dealer/pkg/retrieve/stat.go
@@ -2,12 +2,36 @@ package retrieve
 
 import (
 	"context"
+	_ "encoding/base64"
 	"fmt"
 
+	orbit "github.com/web3eye-io/Web3Eye/dealer/pkg/orbit"
 	retrieverpb "github.com/web3eye-io/Web3Eye/proto/web3eye/retriever/v1"
 )
 
 func (h *Handler) StatRetrieve(ctx context.Context) (*retrieverpb.Retrieve, error) {
-	fmt.Printf("h = %v\n", h)
-	return nil, nil
+	uid := fmt.Sprintf("%v:%v", h.Contract, h.TokenID)
+	index, err := orbit.FileState().GetFileSnapshot(ctx, h.ChainType, uid, h.ChainID)
+	if err != nil {
+		return nil, err
+	}
+	snapshot, err := orbit.Snapshot().GetSnapshot(ctx, index)
+	if err != nil {
+		return nil, err
+	}
+	state, err := orbit.FileState().GetFileRetrieve(ctx, h.ChainType, uid, h.ChainID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &retrieverpb.Retrieve{
+		ChainType:        h.ChainType,
+		ChainID:          h.ChainID,
+		Contract:         h.Contract,
+		TokenID:          h.TokenID,
+		RetrieveState:    state,
+		ProposalCID:      snapshot.ProposalCID,
+		DealID:           snapshot.DealID,
+		BackupPayloadCID: snapshot.SnapshotRoot,
+	}, nil
 }
