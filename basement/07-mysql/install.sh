681288f30bd034068d065d85e5da80f9112b0cae
Geapefurit <670884108@qq.com>
2023-09-04 18:00:43 +0800

    current wrong name for install mysql

--- basement/07-mysql/install.sh
+++ basement/07-mysql/install.sh
@@ -18,10 +18,10 @@ git clone -b branch/web3eye https://github.com/NpoolPlatform/mysql-cluster.git
 sed -i 's/consul_register_enable: "true"/consul_register_enable: "false"/g' $SHELL_FOLDER/mysql-cluste/k8s/01-configmap.yaml
 sed -i 's/pmm_admin_enable: "true"/pmm_admin_enable: "false"/g' $SHELL_FOLDER/mysql-cluste/k8s/01-configmap.yaml          
 
-kubectl apply -f $SHELL_FOLDER/mysql-cluste/mysql-env.yaml
+kubectl apply -f $SHELL_FOLDER/mysql-cluster/mysql-env.yaml
 export MYSQL_ROOT_PASSWORD="web321eye"
-envsubst < $SHELL_FOLDER/mysql-cluste/k8s/secret.yaml | kubectl apply -f -
-kubectl apply -k $SHELL_FOLDER/mysql-cluste/k8s/
+envsubst < $SHELL_FOLDER/mysql-cluster/k8s/secret.yaml | kubectl apply -f -
+kubectl apply -k $SHELL_FOLDER/mysql-cluster/k8s/
 
 sleep 5
 kubectl get pods | grep mysql
