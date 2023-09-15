10f6a6efb71521907b08a4b1db12c6a8fbf7b715
Geapefurit <670884108@qq.com>
2023-09-15 16:39:49 +0800

    change emit default to false

--- entrance/api/v1/token/search.go
+++ entrance/api/v1/token/search.go
@@ -53,7 +53,7 @@ var (
 
 func init() {
 	pbJsonMarshaler = jsonpb.Marshaler{
-		EmitDefaults: true,
+		EmitDefaults: false,
 	}
 	mux := servermux.AppServerMux()
 	mux.HandleFunc("/search/file", Search)
