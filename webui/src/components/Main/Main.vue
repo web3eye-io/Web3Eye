0d0ab1f13d1171b35d86a7d349baf77d5c7dc94a
lidaqiang <dqli161@163.com>
2023-10-08 17:14:53 +0800

    add search box in header

--- webui/src/components/Main/Main.vue
+++ webui/src/components/Main/Main.vue
@@ -17,73 +17,16 @@
       </div>
     </div>
     <div class="column input-container">
-      <input class="upload" id="drop-area" placeholder="search contract address or drag an image here" v-model="contract" />
-      <q-icon name="img:icons/search.png" size="18px" class="search" />
+      <SearchBox />
     </div>
   </div>
 </template>
 
 <script setup lang='ts'>
-import { onMounted, ref } from 'vue'
+import { defineAsyncComponent } from 'vue';
 import largelogo from '../../assets/logo/large-logo.png'
-import { useRouter } from 'vue-router'
-import { useContractStore } from 'src/teststore/contract';
-import { useTokenStore } from 'src/teststore/token';
-const contract = ref('')
-const _contract = useContractStore()
+const SearchBox = defineAsyncComponent(() => import('src/components/Main/SearchBox.vue'))
 
-const getContractAndTokens = (offset: number, limit: number) => {
-  _contract.getContractAndTokens({
-    Contract: contract.value,
-    Offset: offset,
-    Limit: limit,
-    Message: {}
-  }, (error: boolean) => {
-    if (error) return
-    void router.push('/contract')
-  })
-}
-
-const router = useRouter()
-
-const token = useTokenStore()
-
-onMounted(() => {
-  const dropArea = document.getElementById('drop-area')
-  dropArea?.addEventListener('drop', (e) => {
-    e.stopPropagation()
-    e.preventDefault()
-    let formData = new FormData()
-    const file = e.dataTransfer?.files[0]
-    formData.append('UploadFile', file as Blob)
-    formData.append('Limit', '20')
-    contract.value = file?.name as string
-    token.searchTokens(formData, (error: boolean) => {
-      if (!error) {
-        void router.push('/token')
-      }
-    })
-  })
-  dropArea?.addEventListener('dragenter', (e) => {
-    e.stopPropagation()
-    e.preventDefault()
-    console.log('enter')
-  })
-  dropArea?.addEventListener('dragleave', (e) => {
-    e.stopPropagation()
-    e.preventDefault()
-    console.log('leave')
-  })
-  dropArea?.addEventListener('keypress', (e) => {
-    if (e.key != 'Enter') {
-      return
-    }
-    e.stopPropagation()
-    e.preventDefault()
-    if (contract.value?.length === 0) return
-    getContractAndTokens(0, 100)
-  })
-})
 </script>
 
 <style lang='sass' scoped>
@@ -141,22 +84,4 @@ onMounted(() => {
 .upload,.input-container
   margin: 0 auto
   width: 940px
-.upload
-  display: block
-  position: relative
-  width: 940px
-  margin: 0 auto
-  margin-top: 40px
-  padding-left: 40px
-  height: 48px
-  border-radius: 24px
-  border: 1px solid #3187FF
-  &:focus
-    outline: 1px solid #3187FF
-.search
-  display: inline-block
-  position: relative
-  padding-left: 20px
-  line-height: 45px
-  top: -43px
 </style>
