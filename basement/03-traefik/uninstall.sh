bcf85b8330870dc88cbe14bfafc0670babcb8d91
Geapefurit <670884108@qq.com>
2023-10-08 11:33:01 +0800

    delete nfs-storage installtion

---
+++ basement/03-traefik/uninstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+SHELL_FOLDER=$(
+    cd "$(dirname "$0")"
+    pwd
+)
+PROJECT_FOLDER=$(
+    cd $SHELL_FOLDER/../
+    pwd
+)
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+kubectl delete -f $SHELL_FOLDER/06-middlewares.yaml
+
+kubectl delete -k $SHELL_FOLDER
