0b836ae0bb1e626299a6879435a324dbfc6daef2
Geapefurit <670884108@qq.com>
2023-11-17 11:59:18 +0800

    rewrite indexer

--- block-etl/pkg/chains/eth/check.go
+++ block-etl/pkg/chains/eth/check.go
@@ -5,67 +5,25 @@ import (
 	"strings"
 
 	"github.com/web3eye-io/Web3Eye/common/chains/eth"
-	endpointNMCli "github.com/web3eye-io/Web3Eye/nft-meta/pkg/client/v1/endpoint"
-	"github.com/web3eye-io/Web3Eye/proto/web3eye"
-	"github.com/web3eye-io/Web3Eye/proto/web3eye/nftmeta/v1/cttype"
-	"github.com/web3eye-io/Web3Eye/proto/web3eye/nftmeta/v1/endpoint"
 
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
 )
 
 // check endpoints and update it`s state on db
 func (e *EthIndexer) CheckEndpointAndDeal(ctx context.Context) {
-	updateInfos := []*endpoint.EndpointReq{}
+	okEndpints := []string{}
 	// extract wrong endpoints
 	for _, v := range e.Endpoints {
 		_, inspectErr := eth.GetEndpointChainID(ctx, v)
 		if inspectErr != nil {
 			logger.Sugar().Warnf("check the endpoint %v is unavailable,err: %v,has been removed", v, inspectErr)
-			conds := &endpoint.Conds{
-				ChainType: &web3eye.StringVal{
-					Op:    "eq",
-					Value: e.ChainType.String(),
-				},
-				Address: &web3eye.StringVal{
-					Op:    "eq",
-					Value: v,
-				},
-			}
-			getEResp, err := endpointNMCli.GetEndpoints(ctx, &endpoint.GetEndpointsRequest{Conds: conds})
-			if err != nil {
-				logger.Sugar().Warnf("get endpoints from nft-meta failed, err: %v", err)
-				continue
-			}
-			for _, info := range getEResp.GetInfos() {
-				remark := inspectErr.Error()
-				updateInfos = append(updateInfos, &endpoint.EndpointReq{
-					ID:        &info.ID,
-					ChainType: &info.ChainType,
-					ChainID:   &info.ChainID,
-					Address:   &info.Address,
-					State:     cttype.EndpointState_EndpointError.Enum(),
-					Remark:    &remark,
-				})
-			}
+			e.BadEndpoints[v] = inspectErr
+		} else {
+			okEndpints = append(okEndpints, v)
 		}
 	}
 
-	// clean up the map
-	e.BadEndpoints = make(map[string]error)
-
-	// update the infos to db
-	updateEResp, err := endpointNMCli.UpdateEndpoints(ctx, &endpoint.UpdateEndpointsRequest{
-		Infos: updateInfos,
-	})
-	if err != nil {
-		logger.Sugar().Errorf("get endpoints from nft-meta failed, err: %v", err)
-		return
-	}
-	if len(updateInfos) != 0 {
-		for _, v := range updateEResp.Infos {
-			logger.Sugar().Warnf("update endpoint %v failed, err: %v", v.ID, v.MSG)
-		}
-	}
+	e.UpdateEndpoints(okEndpints)
 }
 
 // check if endpoints should be stoped by the err,and return weather to retry again
@@ -78,13 +36,15 @@ func (e *EthIndexer) checkErr(ctx context.Context, err error) {
 	}
 
 	stopErrs := []string{"401 Unauthorized", "429"}
+	needCheck := false
 	for _, v := range stopErrs {
 		if strings.Contains(err.Error(), v) {
-			e.CheckEndpointAndDeal(ctx)
-			if len(e.Endpoints) == 0 {
-				e.StopIndex()
-			}
-			return
+			needCheck = true
+			break
 		}
+	}
+
+	if len(e.Endpoints) == 0 {
+		e.StopIndex()
 	}
 }
