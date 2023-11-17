b35e5d9d024bb763f4748940ec819513db1348fe
Geapefurit <670884108@qq.com>
2023-11-17 15:28:39 +0800

    extract indexer

--- block-etl/pkg/chains/eth/task.go
+++ block-etl/pkg/chains/eth/task.go
@@ -6,16 +6,10 @@ import (
 	"time"
 
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
-	"github.com/apache/pulsar-client-go/pulsar"
-	"github.com/web3eye-io/Web3Eye/common/ctpulsar"
-	"github.com/web3eye-io/Web3Eye/common/utils"
+	"github.com/web3eye-io/Web3Eye/block-etl/pkg/chains/indexer"
 	blockNMCli "github.com/web3eye-io/Web3Eye/nft-meta/pkg/client/v1/block"
-	synctaskNMCli "github.com/web3eye-io/Web3Eye/nft-meta/pkg/client/v1/synctask"
-	ctMessage "github.com/web3eye-io/Web3Eye/proto/web3eye"
 	basetype "github.com/web3eye-io/Web3Eye/proto/web3eye/basetype/v1"
 	blockProto "github.com/web3eye-io/Web3Eye/proto/web3eye/nftmeta/v1/block"
-	"github.com/web3eye-io/Web3Eye/proto/web3eye/nftmeta/v1/cttype"
-	"github.com/web3eye-io/Web3Eye/proto/web3eye/nftmeta/v1/synctask"
 )
 
 const (
@@ -28,152 +22,12 @@ const (
 )
 
 type EthIndexer struct {
-	Endpoints       []string
-	BadEndpoints    map[string]error
-	ChainType       basetype.ChainType
-	ChainID         string
-	CurrentBlockNum uint64
-	onIndex         bool
-	cancel          *context.CancelFunc
+	*indexer.Indexer
 }
 
-func NewIndexer(chainID string) *EthIndexer {
+func NewEthIndexer(chainID string) *EthIndexer {
 	return &EthIndexer{
-		BadEndpoints:    make(map[string]error),
-		ChainType:       basetype.ChainType_Ethereum,
-		ChainID:         chainID,
-		CurrentBlockNum: 0,
-	}
-}
-
-func (e *EthIndexer) StartIndex(ctx context.Context) {
-	logger.Sugar().Infof("start the indexer chainType: %v, chainID: %v", e.ChainType, e.ChainID)
-	ctx, cancel := context.WithCancel(ctx)
-	e.cancel = &cancel
-	go e.GetCurrentBlockNum(ctx, updateBlockNumInterval)
-
-	e.onIndex = true
-
-	taskBlockNum, err := e.PullTaskTopics(ctx)
-	if err != nil {
-		logger.Sugar().Error(err)
-		panic(err)
-	}
-	for i := 0; i < maxParseGoroutineNum; i++ {
-		go e.IndexBlock(ctx, taskBlockNum)
-	}
-}
-
-func (e *EthIndexer) UpdateEndpoints(endpoints []string) {
-	e.Endpoints = endpoints
-}
-
-func (e *EthIndexer) IsOnIndex() bool {
-	return e.onIndex
-}
-
-func (e *EthIndexer) StopIndex() {
-	if e.cancel != nil {
-		logger.Sugar().Infof("stop the indexer chainType: %v, chainID: %v", e.ChainType, e.ChainID)
-		(*e.cancel)()
-		e.cancel = nil
-		e.BadEndpoints = nil
-		e.Endpoints = nil
-		e.onIndex = false
-	}
-}
-
-func (e *EthIndexer) PullTaskTopics(ctx context.Context) (outBlockNum chan uint64, err error) {
-	logger.Sugar().Info("start to index task for ethereum")
-	conds := &synctask.Conds{
-		ChainType: &ctMessage.StringVal{
-			Value: e.ChainType.String(),
-			Op:    "eq",
-		},
-		ChainID: &ctMessage.StringVal{
-			Value: e.ChainID,
-			Op:    "eq",
-		},
-		SyncState: &ctMessage.StringVal{
-			Value: cttype.SyncState_Start.String(),
-			Op:    "eq",
-		},
-	}
-
-	pulsarCli, err := ctpulsar.Client()
-	if err != nil {
-		return nil, err
-	}
-	defer pulsarCli.Close()
-
-	outBlockNum = make(chan uint64)
-	go func() {
-		defer close(outBlockNum)
-		for {
-			select {
-			case <-time.NewTicker(CheckTopicInterval).C:
-				resp, err := synctaskNMCli.GetSyncTasks(ctx, &synctask.GetSyncTasksRequest{Conds: conds, Offset: 0, Limit: maxTopicNum})
-				if err != nil {
-					logger.Sugar().Error(err)
-				}
-
-				for _, v := range resp.GetInfos() {
-					e.indexTopicTasks(ctx, pulsarCli, v, outBlockNum)
-				}
-			case <-ctx.Done():
-				return
-			}
-		}
-	}()
-	return outBlockNum, nil
-}
-
-func (e *EthIndexer) indexTopicTasks(ctx context.Context, pulsarCli pulsar.Client, task *synctask.SyncTask, outBlockNum chan uint64) {
-	output := make(chan pulsar.ConsumerMessage)
-	consumer, err := pulsarCli.Subscribe(pulsar.ConsumerOptions{
-		Topic:            task.Topic,
-		SubscriptionName: "TaskConsummer",
-		Type:             pulsar.Shared,
-		MessageChannel:   output,
-	})
-	if err != nil {
-		logger.Sugar().Errorf("consummer SyncTask failed ,err: %v", err)
-		return
-	}
-	defer consumer.Close()
-
-	retries := 0
-	maxRetries := 3
-	for {
-		select {
-		case msg := <-output:
-			blockNum, err := utils.Bytes2Uint64(msg.Payload())
-			if err != nil {
-				logger.Sugar().Errorf("consummer SyncTask failed ,err: %v", err)
-				continue
-			}
-			outBlockNum <- blockNum
-
-			err = consumer.Ack(msg)
-			if err != nil {
-				logger.Sugar().Errorf("ack SyncTask msg failed ,err: %v", err)
-				continue
-			}
-			retries = 0
-		case <-time.NewTicker(CheckTopicInterval).C:
-			if retries > maxRetries {
-				return
-			}
-			resp, err := synctaskNMCli.TriggerSyncTask(ctx, &synctask.TriggerSyncTaskRequest{Topic: task.Topic, CurrentBlockNum: e.CurrentBlockNum})
-			if err != nil {
-				logger.Sugar().Errorf("triggerSyncTask failed ,err: %v", err)
-				continue
-			}
-			if resp.Info.SyncState != cttype.SyncState_Start {
-				return
-			}
-			retries++
-		}
+		Indexer: indexer.NewIndexer(chainID, basetype.ChainType_Ethereum),
 	}
 }
 
