d5f26f61d226de9cba79c7e0a281d38752a45eae
Geapefurit <670884108@qq.com>
2023-11-17 09:56:17 +0800

    check proxy

--- basement/04-milvus/install.sh
+++ basement/04-milvus/install.sh
@@ -14,5 +14,8 @@ helm install milvus -n kube-system milvus/milvus
 sleep 20
 kubectl get pods -n kube-system | grep milvus
 
+env |grep proxy
+env |grep PROXY
+
 # helm install milvus milvus/milvus --set cpu=1
 # helm install milvus milvus/milvus --set cluster.enabled=false --set etcd.replicaCount=1 --set minio.mode=standalone --set pulsar.enabled=false --set standalone.resources.limits.cpu: 3
