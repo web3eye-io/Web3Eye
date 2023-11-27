ab30d04e63a3ebff1847af912343a089d645dd35
Geapefurit <670884108@qq.com>
2023-11-27 10:50:17 +0800

    helm pull then install

--- basement/04-milvus/install.sh
+++ basement/04-milvus/install.sh
@@ -7,15 +7,16 @@ SHELL_FOLDER=$(
 )
 env |grep proxy
 env |grep PROXY
-helm repo add milvus https://milvus-io.github.io/milvus-helm/
-helm repo update
 
-helm install milvus -n kube-system milvus/milvus
+MILVUS_CHART_VERSION=4.0.31
+
+helm repo add milvus https://milvus-io.github.io/milvus-helm/
+helm pull milvus/milvus --version $MILVUS_CHART_VERSION
+
+helm install milvus -n kube-system ./milvus-$MILVUS_CHART_VERSION.tgz 
 # have too many part,so wait more time
 sleep 20
 kubectl get pods -n kube-system | grep milvus
-
-
 
 # helm install milvus milvus/milvus --set cpu=1
 # helm install milvus milvus/milvus --set cluster.enabled=false --set etcd.replicaCount=1 --set minio.mode=standalone --set pulsar.enabled=false --set standalone.resources.limits.cpu: 3
