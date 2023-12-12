0c16e2caf52a04eb03440011d6c5bbfa8383577f
Geapefurit <670884108@qq.com>
2023-12-12 15:27:11 +0800

    implement updateEndpoints for block-etl

--- nft-meta/api/v1/endpoint/endpoint.go
+++ nft-meta/api/v1/endpoint/endpoint.go
@@ -10,16 +10,16 @@ import (
 
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
 	basetype "github.com/web3eye-io/Web3Eye/proto/web3eye/basetype/v1"
-	npool "github.com/web3eye-io/Web3Eye/proto/web3eye/nftmeta/v1/endpoint"
+	endpointproto "github.com/web3eye-io/Web3Eye/proto/web3eye/nftmeta/v1/endpoint"
 )
 
-func (s *Server) CreateEndpoint(ctx context.Context, in *npool.CreateEndpointRequest) (*npool.CreateEndpointResponse, error) {
+func (s *Server) CreateEndpoint(ctx context.Context, in *endpointproto.CreateEndpointRequest) (*endpointproto.CreateEndpointResponse, error) {
 	if req := in.GetInfo(); req == nil {
 		logger.Sugar().Errorw(
 			"CreateEndpoint",
 			"In", in,
 		)
-		return &npool.CreateEndpointResponse{}, status.Error(codes.InvalidArgument, "Info is empty")
+		return &endpointproto.CreateEndpointResponse{}, status.Error(codes.InvalidArgument, "Info is empty")
 	}
 
 	in.GetInfo().ChainID = nil
@@ -34,24 +34,24 @@ func (s *Server) CreateEndpoint(ctx context.Context, in *npool.CreateEndpointReq
 	)
 	if err != nil {
 		logger.Sugar().Errorw("CreateEndpoint", "error", err)
-		return &npool.CreateEndpointResponse{}, status.Error(codes.Internal, err.Error())
+		return &endpointproto.CreateEndpointResponse{}, status.Error(codes.Internal, err.Error())
 	}
 
 	info, err := h.CreateEndpoint(ctx)
 	if err != nil {
 		logger.Sugar().Errorw("CreateEndpoint", "error", err)
-		return &npool.CreateEndpointResponse{}, status.Error(codes.Internal, err.Error())
+		return &endpointproto.CreateEndpointResponse{}, status.Error(codes.Internal, err.Error())
 	}
 
-	return &npool.CreateEndpointResponse{
+	return &endpointproto.CreateEndpointResponse{
 		Info: info,
 	}, nil
 }
 
-func (s *Server) CreateEndpoints(ctx context.Context, in *npool.CreateEndpointsRequest) (*npool.CreateEndpointsResponse, error) {
+func (s *Server) CreateEndpoints(ctx context.Context, in *endpointproto.CreateEndpointsRequest) (*endpointproto.CreateEndpointsResponse, error) {
 	if len(in.GetInfos()) == 0 {
 		logger.Sugar().Errorw("CreateEndpoints", "error", "Infos is empty")
-		return &npool.CreateEndpointsResponse{}, status.Error(codes.InvalidArgument, "Infos is empty")
+		return &endpointproto.CreateEndpointsResponse{}, status.Error(codes.InvalidArgument, "Infos is empty")
 	}
 
 	h, err := handler.NewHandler(ctx,
@@ -59,26 +59,26 @@ func (s *Server) CreateEndpoints(ctx context.Context, in *npool.CreateEndpointsR
 	)
 	if err != nil {
 		logger.Sugar().Errorw("CreateEndpoints", "error", err)
-		return &npool.CreateEndpointsResponse{}, status.Error(codes.Internal, err.Error())
+		return &endpointproto.CreateEndpointsResponse{}, status.Error(codes.Internal, err.Error())
 	}
 	infos, err := h.CreateEndpoints(ctx)
 	if err != nil {
 		logger.Sugar().Errorw("CreateEndpoints", "error", err)
-		return &npool.CreateEndpointsResponse{}, status.Error(codes.Internal, err.Error())
+		return &endpointproto.CreateEndpointsResponse{}, status.Error(codes.Internal, err.Error())
 	}
 
-	return &npool.CreateEndpointsResponse{
+	return &endpointproto.CreateEndpointsResponse{
 		Infos: infos,
 	}, nil
 }
 
-func (s *Server) UpdateEndpoint(ctx context.Context, in *npool.UpdateEndpointRequest) (*npool.UpdateEndpointResponse, error) {
+func (s *Server) UpdateEndpoint(ctx context.Context, in *endpointproto.UpdateEndpointRequest) (*endpointproto.UpdateEndpointResponse, error) {
 	if req := in.GetInfo(); req == nil {
 		logger.Sugar().Errorw(
 			"UpdateEndpoint",
 			"In", in,
 		)
-		return &npool.UpdateEndpointResponse{}, status.Error(codes.InvalidArgument, "Info is empty")
+		return &endpointproto.UpdateEndpointResponse{}, status.Error(codes.InvalidArgument, "Info is empty")
 	}
 	h, err := handler.NewHandler(ctx,
 		handler.WithID(in.Info.ID, true),
@@ -90,40 +90,84 @@ func (s *Server) UpdateEndpoint(ctx context.Context, in *npool.UpdateEndpointReq
 	)
 	if err != nil {
 		logger.Sugar().Errorw("UpdateEndpoint", "ID", in.GetInfo().GetID(), "error", err)
-		return &npool.UpdateEndpointResponse{}, status.Error(codes.Internal, err.Error())
+		return &endpointproto.UpdateEndpointResponse{}, status.Error(codes.Internal, err.Error())
 	}
 
 	info, err := h.UpdateEndpoint(ctx)
 	if err != nil {
 		logger.Sugar().Errorw("UpdateEndpoint", "ID", in.GetInfo().GetID(), "error", err)
-		return &npool.UpdateEndpointResponse{}, status.Error(codes.Internal, err.Error())
+		return &endpointproto.UpdateEndpointResponse{}, status.Error(codes.Internal, err.Error())
 	}
 
-	return &npool.UpdateEndpointResponse{
+	return &endpointproto.UpdateEndpointResponse{
 		Info: info,
 	}, nil
 }
 
-func (s *Server) GetEndpoint(ctx context.Context, in *npool.GetEndpointRequest) (*npool.GetEndpointResponse, error) {
+func (s *Server) UpdateEndpoints(ctx context.Context, in *endpointproto.UpdateEndpointsRequest) (*endpointproto.UpdateEndpointsResponse, error) {
+	if len(in.Infos) == 0 {
+		logger.Sugar().Errorw(
+			"UpdateEndpoints",
+			"In", in,
+			"Msg", "have no input",
+		)
+		return &endpointproto.UpdateEndpointsResponse{}, status.Error(codes.InvalidArgument, "Infos is empty")
+	}
+
+	failedInfos := []*endpointproto.FailedInfo{}
+	for _, info := range in.Infos {
+		h, err := handler.NewHandler(ctx,
+			handler.WithID(info.ID, true),
+			handler.WithChainType(info.ChainType, false),
+			handler.WithChainID(info.ChainID, false),
+			handler.WithAddress(info.Address, false),
+			handler.WithState(info.State, false),
+			handler.WithRemark(info.Remark, false),
+		)
+		if err != nil {
+			failedInfos = append(failedInfos, &endpointproto.FailedInfo{
+				ID:  *info.ID,
+				MSG: err.Error(),
+			})
+			logger.Sugar().Errorw("UpdateEndpoints", "ID", info.GetID(), "error", err)
+			continue
+		}
+
+		_, err = h.UpdateEndpoint(ctx)
+		if err != nil {
+			failedInfos = append(failedInfos, &endpointproto.FailedInfo{
+				ID:  *info.ID,
+				MSG: err.Error(),
+			})
+			logger.Sugar().Errorw("UpdateEndpoints", "ID", info.GetID(), "error", err)
+		}
+	}
+
+	return &endpointproto.UpdateEndpointsResponse{
+		Infos: failedInfos,
+	}, nil
+}
+
+func (s *Server) GetEndpoint(ctx context.Context, in *endpointproto.GetEndpointRequest) (*endpointproto.GetEndpointResponse, error) {
 	h, err := handler.NewHandler(ctx,
 		handler.WithID(&in.ID, true),
 	)
 	if err != nil {
 		logger.Sugar().Errorw("GetEndpoint", "ID", in.GetID(), "error", err)
-		return &npool.GetEndpointResponse{}, status.Error(codes.Internal, err.Error())
+		return &endpointproto.GetEndpointResponse{}, status.Error(codes.Internal, err.Error())
 	}
 	info, err := h.GetEndpoint(ctx)
 	if err != nil {
 		logger.Sugar().Errorw("GetEndpoint", "ID", in.GetID(), "error", err)
-		return &npool.GetEndpointResponse{}, status.Error(codes.Internal, err.Error())
+		return &endpointproto.GetEndpointResponse{}, status.Error(codes.Internal, err.Error())
 	}
 
-	return &npool.GetEndpointResponse{
+	return &endpointproto.GetEndpointResponse{
 		Info: info,
 	}, nil
 }
 
-func (s *Server) GetEndpointOnly(ctx context.Context, in *npool.GetEndpointOnlyRequest) (*npool.GetEndpointOnlyResponse, error) {
+func (s *Server) GetEndpointOnly(ctx context.Context, in *endpointproto.GetEndpointOnlyRequest) (*endpointproto.GetEndpointOnlyResponse, error) {
 	h, err := handler.NewHandler(ctx,
 		handler.WithConds(in.Conds),
 		handler.WithOffset(0),
@@ -131,26 +175,26 @@ func (s *Server) GetEndpointOnly(ctx context.Context, in *npool.GetEndpointOnlyR
 	)
 	if err != nil {
 		logger.Sugar().Errorw("GetEndpointOnly", "error", err)
-		return &npool.GetEndpointOnlyResponse{}, status.Error(codes.Internal, err.Error())
+		return &endpointproto.GetEndpointOnlyResponse{}, status.Error(codes.Internal, err.Error())
 	}
 	infos, total, err := h.GetEndpoints(ctx)
 	if err != nil {
 		logger.Sugar().Errorw("GetEndpointOnly", "error", err)
-		return &npool.GetEndpointOnlyResponse{}, status.Error(codes.Internal, err.Error())
+		return &endpointproto.GetEndpointOnlyResponse{}, status.Error(codes.Internal, err.Error())
 	}
 
 	if total != 1 {
 		errMsg := "more than one result or have no result"
 		logger.Sugar().Errorw("GetEndpointOnly", "error", errMsg)
-		return &npool.GetEndpointOnlyResponse{}, status.Error(codes.Internal, errMsg)
+		return &endpointproto.GetEndpointOnlyResponse{}, status.Error(codes.Internal, errMsg)
 	}
 
-	return &npool.GetEndpointOnlyResponse{
+	return &endpointproto.GetEndpointOnlyResponse{
 		Info: infos[0],
 	}, nil
 }
 
-func (s *Server) GetEndpoints(ctx context.Context, in *npool.GetEndpointsRequest) (*npool.GetEndpointsResponse, error) {
+func (s *Server) GetEndpoints(ctx context.Context, in *endpointproto.GetEndpointsRequest) (*endpointproto.GetEndpointsResponse, error) {
 	h, err := handler.NewHandler(ctx,
 		handler.WithConds(in.Conds),
 		handler.WithOffset(in.Offset),
@@ -158,76 +202,76 @@ func (s *Server) GetEndpoints(ctx context.Context, in *npool.GetEndpointsRequest
 	)
 	if err != nil {
 		logger.Sugar().Errorw("GetEndpoints", "error", err)
-		return &npool.GetEndpointsResponse{}, status.Error(codes.Internal, err.Error())
+		return &endpointproto.GetEndpointsResponse{}, status.Error(codes.Internal, err.Error())
 	}
 	infos, total, err := h.GetEndpoints(ctx)
 	if err != nil {
 		logger.Sugar().Errorw("GetEndpoints", "error", err)
-		return &npool.GetEndpointsResponse{}, status.Error(codes.Internal, err.Error())
+		return &endpointproto.GetEndpointsResponse{}, status.Error(codes.Internal, err.Error())
 	}
 
-	return &npool.GetEndpointsResponse{
+	return &endpointproto.GetEndpointsResponse{
 		Infos: infos,
 		Total: total,
 	}, nil
 }
 
-func (s *Server) ExistEndpoint(ctx context.Context, in *npool.ExistEndpointRequest) (*npool.ExistEndpointResponse, error) {
+func (s *Server) ExistEndpoint(ctx context.Context, in *endpointproto.ExistEndpointRequest) (*endpointproto.ExistEndpointResponse, error) {
 	h, err := handler.NewHandler(ctx,
 		handler.WithID(&in.ID, true),
 	)
 	if err != nil {
 		logger.Sugar().Errorw("ExistEndpoint", "ID", in.GetID(), "error", err)
-		return &npool.ExistEndpointResponse{}, status.Error(codes.Internal, err.Error())
+		return &endpointproto.ExistEndpointResponse{}, status.Error(codes.Internal, err.Error())
 	}
 
 	exist, err := h.ExistEndpoint(ctx)
 	if err != nil {
 		logger.Sugar().Errorw("ExistEndpoint", "ID", in.GetID(), "error", err)
-		return &npool.ExistEndpointResponse{}, status.Error(codes.Internal, err.Error())
+		return &endpointproto.ExistEndpointResponse{}, status.Error(codes.Internal, err.Error())
 	}
 
-	return &npool.ExistEndpointResponse{
+	return &endpointproto.ExistEndpointResponse{
 		Exist: exist,
 	}, nil
 }
 
-func (s *Server) ExistEndpointConds(ctx context.Context, in *npool.ExistEndpointCondsRequest) (*npool.ExistEndpointCondsResponse, error) {
+func (s *Server) ExistEndpointConds(ctx context.Context, in *endpointproto.ExistEndpointCondsRequest) (*endpointproto.ExistEndpointCondsResponse, error) {
 	h, err := handler.NewHandler(ctx,
 		handler.WithConds(in.Conds),
 	)
 	if err != nil {
 		logger.Sugar().Errorw("ExistEndpointConds", "error", err)
-		return &npool.ExistEndpointCondsResponse{}, status.Error(codes.Internal, err.Error())
+		return &endpointproto.ExistEndpointCondsResponse{}, status.Error(codes.Internal, err.Error())
 	}
 
 	exist, err := h.ExistEndpointConds(ctx)
 	if err != nil {
 		logger.Sugar().Errorw("ExistEndpointConds", "error", err)
-		return &npool.ExistEndpointCondsResponse{}, status.Error(codes.Internal, err.Error())
+		return &endpointproto.ExistEndpointCondsResponse{}, status.Error(codes.Internal, err.Error())
 	}
 
-	return &npool.ExistEndpointCondsResponse{
+	return &endpointproto.ExistEndpointCondsResponse{
 		Exist: exist,
 	}, nil
 }
 
-func (s *Server) DeleteEndpoint(ctx context.Context, in *npool.DeleteEndpointRequest) (*npool.DeleteEndpointResponse, error) {
+func (s *Server) DeleteEndpoint(ctx context.Context, in *endpointproto.DeleteEndpointRequest) (*endpointproto.DeleteEndpointResponse, error) {
 	h, err := handler.NewHandler(ctx,
 		handler.WithID(&in.ID, true),
 	)
 	if err != nil {
 		logger.Sugar().Errorw("DeleteEndpoint", "ID", in.GetID(), "error", err)
-		return &npool.DeleteEndpointResponse{}, status.Error(codes.InvalidArgument, err.Error())
+		return &endpointproto.DeleteEndpointResponse{}, status.Error(codes.InvalidArgument, err.Error())
 	}
 
 	info, err := h.DeleteEndpoint(ctx)
 	if err != nil {
 		logger.Sugar().Errorw("DeleteEndpoint", "ID", in.GetID(), "error", err)
-		return &npool.DeleteEndpointResponse{}, status.Error(codes.Internal, err.Error())
+		return &endpointproto.DeleteEndpointResponse{}, status.Error(codes.Internal, err.Error())
 	}
 
-	return &npool.DeleteEndpointResponse{
+	return &endpointproto.DeleteEndpointResponse{
 		Info: info,
 	}, nil
 }
