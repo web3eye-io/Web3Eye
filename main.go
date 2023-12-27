d3ef77135f8898f48a5a953e645eb0c9a43ed8bb
Geapefurit <670884108@qq.com>
2023-12-27 11:01:32 +0800

    fix bug for nil panic

--- main.go
+++ main.go
@@ -3,91 +3,35 @@ package main
 // import (
 // 	"context"
 // 	"fmt"
-// 	"os"
-// 	"time"
+// 	"math/big"
 
 // 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
-// 	"github.com/apache/pulsar-client-go/pulsar"
-// 	"github.com/web3eye-io/Web3Eye/common/ctpulsar"
+// 	"github.com/ethereum/go-ethereum"
+// 	"github.com/ethereum/go-ethereum/common"
+// 	"github.com/ethereum/go-ethereum/ethclient"
+// 	"github.com/web3eye-io/Web3Eye/common/chains/eth"
+// 	"github.com/web3eye-io/Web3Eye/common/utils"
 // )
 
 // func main() {
 // 	logger.Init(logger.DebugLevel, "./a.log")
-
-// 	go produce()
-// 	consumer()
-// }
-// func consumer() {
-// 	cli, err := ctpulsar.Client()
-// 	if err != nil {
-// 		logger.Sugar().Error(err)
-// 		os.Exit(0)
-// 	}
-// 	defer cli.Close()
-
-// 	consumChan := make(chan pulsar.ConsumerMessage)
-
-// 	_, err = cli.Subscribe(pulsar.ConsumerOptions{
-// 		Topic:            "token-image-bucket",
-// 		SubscriptionName: "ssss",
-// 		MessageChannel:   consumChan,
+// 	cli, err := ethclient.DialContext(context.Background(), "https://mainnet.infura.io/v3/8cc70eaecd7c40d9817b6f4747f0e2f7")
+// 	fmt.Println(err)
+// 	ret, err := cli.FilterLogs(context.Background(), ethereum.FilterQuery{
+// 		FromBlock: big.NewInt(18874059),
+// 		ToBlock:   big.NewInt(18874059),
+// 		Topics: [][]common.Hash{
+// 			eth.TransfersTopics,
+// 			eth.OrderFulfilledTopics,
+// 		},
 // 	})
-// 	if err != nil {
-// 		logger.Sugar().Error(err)
-// 		os.Exit(0)
-// 	}
+// 	fmt.Println(err)
+// 	fmt.Println(utils.PrettyStruct(ret))
 
-// 	for {
-// 		num := <-consumChan
-// 		fmt.Println(num.Key())
-// 		// if num.Key() == "5" {
-// 		// 	continue
-// 		// }
-// 		// consum.AckID(num.ID())
-// 	}
-// }
-
-// func produce() {
-// 	cli, err := ctpulsar.Client()
-// 	if err != nil {
-// 		logger.Sugar().Error(err)
-// 		os.Exit(0)
-// 	}
-// 	defer cli.Close()
-// 	producer, err := cli.CreateProducer(pulsar.ProducerOptions{
-// 		Topic: "token-image-bucket",
-// 	})
-// 	if err != nil {
-// 		logger.Sugar().Error(err)
-// 		os.Exit(0)
-// 	}
-// 	for i := 0; i < 10; i++ {
-// 		_, err := producer.Send(context.Background(), &pulsar.ProducerMessage{
-// 			Key: fmt.Sprintf("%v", i),
-// 		})
-// 		if err != nil {
-// 			logger.Sugar().Error(err)
-// 			os.Exit(0)
-// 		}
-// 		time.Sleep(time.Second)
-// 	}
-// 	fmt.Println("I finish produce")
-// }
-
-// func tableView() {
-// 	cli, err := ctpulsar.Client()
-// 	if err != nil {
-// 		logger.Sugar().Error(err)
-// 		os.Exit(0)
-// 	}
-// 	defer cli.Close()
-// 	tableView, err := cli.CreateTableView(pulsar.TableViewOptions{
-// 		Topic: "token-image-bucket",
-// 	})
-// 	if err != nil {
-// 		logger.Sugar().Error(err)
-// 		os.Exit(0)
-// 	}
-// 	fmt.Println(tableView.Keys())
-// 	fmt.Println(tableView.Size())
+// 	ecli, err := eth.Client([]string{"https://mainnet.infura.io/v3/8cc70eaecd7c40d9817b6f4747f0e2f7"})
+// 	fmt.Println(err)
+// 	eret, err := ecli.FilterLogsForTopics(context.Background(), 18874059, 18874059, [][]common.Hash{
+// 		eth.TransfersTopics,
+// 		eth.OrderFulfilledTopics})
+// 	fmt.Println(utils.PrettyStruct(eret))
 // }
