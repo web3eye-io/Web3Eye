c57936218a7d1bde1fb472dd273b904f4488aac2
Geapefurit <670884108@qq.com>
2024-04-08 02:08:13 +0000

    update script for gen-car

--- gen-car/script/build-docker-image.sh
+++ gen-car/script/build-docker-image.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
-SHELL_FOLDER=$(
-    cd "$(dirname "$0")"
-    pwd
-)
-PROJECT_FOLDER=$(
-    cd $SHELL_FOLDER/../
-    pwd
-)
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+PROJECT_FOLDER=$(cd $SHELL_FOLDER/../;pwd)
+ROOT_FOLDER=$(cd $PROJECT_FOLDER/../;pwd)
+
 set -o errexit
 set -o nounset
 set -o pipefail
