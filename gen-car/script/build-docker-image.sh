761037a66c5d333112fe12e2f603485cda55a42f
root <670884108@qq.com>
2024-04-15 03:27:39 +0000

    delete debug info

--- gen-car/script/build-docker-image.sh
+++ gen-car/script/build-docker-image.sh
@@ -66,7 +66,7 @@ cd $output_d
 
 user=$(whoami)
 if [ "$user" == "root" ]; then
-    docker build -t ${registry}/${OrginazeName}/$service_name:$version . --build-arg http_proxy='http://172.16.31.202:20171'
+    docker build -t ${registry}/${OrginazeName}/$service_name:$version . 
 else
-    sudo docker build -t ${registry}/${OrginazeName}/$service_name:$version . --build-arg http_proxy='http://172.16.31.202:20171'
+    sudo docker build -t ${registry}/${OrginazeName}/$service_name:$version . 
 fi
